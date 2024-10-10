cc5a59de08a9e63f41c747b6e3bed80a3dc8124d
lodity <lodity9@gmail.com>
2024-10-10 19:42:51 +0300

    style: added prettier plugin

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,12 +1,15 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
+import prettierPlugin from 'eslint-plugin-prettier'
 
 export default [
   {
     files: ['**/*.{js,mjs,cjs,ts}'],
+    plugins: { prettier: prettierPlugin },
     rules: {
       'prefer-const': 'error',
+      'prettier/prettier': 'error',
     },
   },
   { languageOptions: { globals: globals.browser } },
